3387dc385436889b7a88cab6cbd5ba9756656545
Muhammad Kemal Arif <kemalarif92@gmail.com>
2023-04-28 23:08:52 +0800

    menambahkan constraint catatan yang tak ber owner

---
+++ migrations/1682692464928_add-foreign-key-to-owner-column.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  // membuat user baru
+  pgm.sql('INSERT INTO users(id,username,password,fullname)'+
+  ' VALUES (\'old_notes\',\'old_notes\',\'old_notes\',\'old_notes\')');
+
+  // mengubah nilai owner pada note yang ownernya bernilai NULL
+  pgm.sql('UPDATE notes SET owner = \'old_notes\' WHERE owner IS NULL');
+
+  /* memberikan contraint foreign key pada owner terhadap kolom
+   id dari tabel users*/
+  pgm.addConstraint('notes', 'fk_notes.owner_users.id,',
+      'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {};
